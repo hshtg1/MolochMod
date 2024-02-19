f1747b79c5fca279ad87ac34f397748a444a2878
Mateusz Wojtowicz <xylek16@gmail.com>
2024-02-19 21:01:17 +0100

    Quick fix of pocket item

--- scripts/dansemacabre.lua
+++ scripts/dansemacabre.lua
@@ -1,7 +1,11 @@
 local sfx = SFXManager()
 local DANSE_MACABRE_ITEM_ID = Isaac.GetItemIdByName("Danse Macabre")
+local molochType = Isaac.GetPlayerTypeByName("Moloch", false)
 
 function MolochMod:InitializeDanseMacabre(player)
+    if player:GetPlayerType() ~= molochType then
+        return -- End the function early. The below code doesn't run, as long as the player isn't Moloch.
+    end
     player:SetPocketActiveItem(DANSE_MACABRE_ITEM_ID, ActiveSlot.SLOT_POCKET, true)
 end
 
